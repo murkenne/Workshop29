bbb074f4f47cd109ebc64aa436220c2000997756
Kenneth Murray <kwmwins@gmail.com>
2023-08-11 11:36:33 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom'; // Import ReactDOM
+import { createRoot } from 'react-dom/client'; // Import createRoot from 'react-dom' directly
 import App from './App';
 import './index.css';
 
 const rootElement = document.getElementById('root');
 
-ReactDOM.createRoot(rootElement).render(
+createRoot(rootElement).render( // Use createRoot directly without ReactDOM
   <React.StrictMode>
     <BrowserRouter>
       <App />
